cb33b300dc7bebc45d5fe7f2ead9be62b365a784
waraneymmbu <waraney.maikel@ti.ukdw.ac.id>
2025-03-09 23:03:43 +0800

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,6 @@
+celsius_to_fahrenheit = lambda c: (9/5) * c + 32
+celsius_to_reamur = lambda c: 0.8 * c
+
+print(celsius_to_fahrenheit(100))
+print(celsius_to_fahrenheit(0))
+print(celsius_to_reamur(80))
